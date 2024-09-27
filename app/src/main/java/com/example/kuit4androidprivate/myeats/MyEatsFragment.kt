bc8053d83e5fe6b29496bdb26e4d2aa8e4578acd
lolloipop <annie.yubin@gmail.com>
2024-09-28 00:09:38 +0900

    1주차 미션 3조 정유빈 2주차 미션 3조 정유빈

--- app/src/main/java/com/example/kuit4androidprivate/myeats/MyEatsFragment.kt
+++ app/src/main/java/com/example/kuit4androidprivate/myeats/MyEatsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.kuit4androidprivate.databinding.FragmentMyEatsBinding
 
+
 class MyEatsFragment : Fragment() {
 
     private lateinit var binding : FragmentMyEatsBinding
@@ -21,3 +22,66 @@ class MyEatsFragment : Fragment() {
     }
 
 }
+
+override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    // context와 Toast 사용
+    binding.btnHome.setOnClickListener {
+        Toast.makeText(requireContext(), "홈 버튼 클릭됨", Toast.LENGTH_SHORT).show()
+    }
+
+    binding.btnSearch.setOnClickListener {
+        Toast.makeText(requireContext(), "검색 버튼 클릭됨", Toast.LENGTH_SHORT).show()
+    }
+
+    binding.btnFavorite.setOnClickListener {
+        Toast.makeText(requireContext(), "즐겨찾기 버튼 클릭됨", Toast.LENGTH_SHORT).show()
+    }
+
+    binding.btnOrderHistory.setOnClickListener {
+        Toast.makeText(requireContext(), "주문내역 버튼 클릭됨", Toast.LENGTH_SHORT).show()
+    }
+
+    binding.btnMyEats.setOnClickListener {
+        Toast.makeText(requireContext(), "My 이츠 버튼 클릭됨", Toast.LENGTH_SHORT).show()
+    }
+}
+
+override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
+}
+}
+class MyEatsFragment : Fragment() {
+
+    private var _binding: FragmentMyEatsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMyEatsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.btnFavorite.setOnClickListener {
+            val intent = Intent(activity, activity_keep::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
+
+
+
